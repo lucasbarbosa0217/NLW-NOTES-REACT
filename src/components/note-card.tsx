25914ba761323f21f4a45f75f9935538f41c2075
lucasbarbosa0217 <lucasbarbosa0217@gmail.com>
2024-02-10 16:57:13 -0300

    Aula 1 NLW
    - Criada a estrutura básica dos componentes da tela inicial

---
+++ src/components/note-card.tsx
@@ -0,0 +1,14 @@
+export function NoteCard(){
+    return (
+
+           <button className=" text-left rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-500">
+          <span className="text-sm font-medium text-slate-300">há 4 dias</span>
+          <p className="text-sm leading-6 text-slate-400">
+         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore quia sit veniam atque eaque, voluptatibus illum praesentium molestias obcaecati officiis quibusdam numquam architecto optio vero? Hic rerum similique quo repellendus?. 
+          </p>
+
+          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"/>
+        </button>
+   
+    )
+}
